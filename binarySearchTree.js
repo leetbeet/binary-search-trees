8bcd2389545fa59b038053505242002b0d6e15a6
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-09-22 09:23:11 +0800

    Create Tree class and method to build tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -5,3 +5,21 @@ class Node {
     this.right = null;
   }
 }
+
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree([...new Set(array.sort((a, b) => a - b))]);
+  }
+
+  buildTree(array) {
+    if (array.length === 0) return null;
+
+    const mid = Math.floor((array.length - 1) / 2);
+    const root = new Node(array[mid]);
+
+    root.left = this.buildTree(array.slice(0, mid));
+    root.right = this.buildTree(array.slice(mid + 1));
+
+    return root;
+  }
+}
